b1530e44a7a5bb2660606c6f45363c931255668b
Saravanakumar <saravanakumar182005@gmail.com>
2026-01-27 11:23:41 +0530

    Update Drawx.jsx

--- thulir-app/src/Pages/Drawx.jsx
+++ thulir-app/src/Pages/Drawx.jsx
@@ -33,7 +33,7 @@ const eduClosedMixin = (theme) => ({
   transition: theme.transitions.create('width', {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
-  }),
+    }),
   overflowX: 'hidden',
   width: `calc(${theme.spacing(7)} + 1px)`,
   backgroundColor: '#1a3e36',
